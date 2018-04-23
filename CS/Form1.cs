c434991fd2984358f1b821f393c198fcf7b8549f
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/Form1.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using DevExpress.XtraScheduler;
+
+namespace AptTooltipDrag {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+
+        private void schedulerControl1_AppointmentDrag(object sender, DevExpress.XtraScheduler.AppointmentDragEventArgs e) {
+            if (e.HitInterval.Start.TimeOfDay.Hours <= 9) {
+                this.toolTipController1.ShowHint("You cannot move appointment to this area");
+                Application.DoEvents();
+            } else
+                this.toolTipController1.HideHint();
+        }
+
+        private void schedulerControl1_AppointmentDrop(object sender, DevExpress.XtraScheduler.AppointmentDragEventArgs e) {
+            if (e.HitInterval.Start.TimeOfDay.Hours <= 9) {
+                this.toolTipController1.ShowHint("Operation is cancelled");
+                e.Allow = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Appointment apt = this.schedulerStorage1.CreateAppointment(AppointmentType.Normal);
+            apt.Start = DateTime.Now.Date.AddHours(12);
+            apt.Subject = "Sample Appointment";
+            this.schedulerStorage1.Appointments.Add(apt);
+            this.schedulerControl1.ActiveViewType = SchedulerViewType.Day;
+            this.schedulerControl1.Start = DateTime.Now.Date;
+            this.schedulerControl1.DayView.TopRowTime = new TimeSpan(8, 0, 0);
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to display a warning tooltip when dragging an appointment to forbidden area
+
+
+<p>This example illustrates the capability to define locked areas within the current view and display a tooltip message when the end-user tries to drag an appointment there.<br />
+To accomplish this task, two events (SchedulerControl.AppointmentDrag and SchedulerControl.AppointmentDrop) are handled. To display a tooltip, the ToolTipController.ShowHint method is used.</p>
+
+<br/>
+
+
